b10ddfbfda563f6bd72dd40c1d045c07f0c86761
Abir-Hossain <abirhossain9@iut-dhaka.edu>
2022-09-20 22:25:46 +0600

    Added route for post

--- Basics/router.js
+++ Basics/router.js
@@ -6,5 +6,9 @@ const { getCV } = require("./controllers/CvController");
 const fs = require("fs");
 
 router.get("/", getCV);
+router.get("/form", (req, res) => {
+  res.render("form");
+});
+router.post("/form", getCV);
 
 module.exports = router;
